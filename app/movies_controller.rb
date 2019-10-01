8e2ef2eb76c34409d1a83e7c068665c67e5a3dd8
Laura Berge <lauraeberge@gmail.com>
2019-10-01 23:41:33 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,10 @@ end
 
 def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
 
-  Movie.create(args)
+  Movie.create do |m|
+    m.title = args["title"]
+    m.release_date = args["release_date"]
+  end
 end
 
 def can_get_the_first_item_in_the_database
